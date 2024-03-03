b201edb05d963de2001f6bced69a3d4295b266c3
Kunjan <choudharykunjan@gmail.com>
2024-03-04 01:26:46 +0530

    added form validation

--- src/components/userForm/userForm.jsx
+++ src/components/userForm/userForm.jsx
@@ -5,7 +5,7 @@ import Heading from "../heading/heading";
 import { Link } from 'react-router-dom';
 
 
-const UserForm = ({ initialFormData, onFormSubmit,formText }) => {
+const UserForm = ({ initialFormData, onFormSubmit, formText }) => {
 
   const [formData, setFormData] = useState(initialFormData || {
     ID: '',
@@ -13,7 +13,11 @@ const UserForm = ({ initialFormData, onFormSubmit,formText }) => {
     email: '',
     role: ''
   });
- 
+  const [errors, setErrors] = useState({
+    username: '',
+    email: '',
+    role: ''
+  });
 
 
   useEffect(() => {
@@ -31,8 +35,51 @@ const UserForm = ({ initialFormData, onFormSubmit,formText }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onFormSubmit(formData);
+    if (validateForm()) {
+      onFormSubmit(formData);
+      history.push('/');
+    }
   };
+
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = {};
+
+    // Validate ID only if create user 
+    if(formText === "Create New User"){
+      if (!formData.ID.trim()) {
+        newErrors.ID = 'ID is required';
+        valid = false;
+      } 
+    }
+  
+
+    // Validate username
+    if (!formData.username.trim()) {
+      newErrors.username = 'Username is required';
+      valid = false;
+    }
+
+    // Validate email
+    if (!formData.email.trim()) {
+      newErrors.email = 'Email is required';
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = 'Invalid email format';
+      valid = false;
+    }
+
+    // Validate role
+    if (!formData.role.trim()) {
+      newErrors.role = 'Role is required';
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
 
   return (
     <div className="form-container">
@@ -41,23 +88,27 @@ const UserForm = ({ initialFormData, onFormSubmit,formText }) => {
       <form onSubmit={handleSubmit}>
         <div className="user-creation-form-group">
           <label htmlFor="ID">ID:</label>
-          <input type="text" id="ID" name="ID" value={formData.ID} onChange={handleChange} disabled={formText === "Update User"}/>
+          <input type="text" id="ID" name="ID" value={formData.ID} onChange={handleChange} disabled={formText === "Update User"} />
+          {errors.ID && <div className="error">{errors.ID}</div>}
         </div>
         <div className="user-creation-form-group">
           <label htmlFor="username">Username:</label>
           <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} />
+          {errors.username && <div className="error">{errors.username}</div>}
         </div>
         <div className="user-creation-form-group">
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" required name="email" value={formData.email} onChange={handleChange} />
+          {errors.email && <div className="error">{errors.email}</div>}
         </div>
         <div className="user-creation-form-group">
           <label htmlFor="role">Role:</label>
           <select id="role" name="role" value={formData.role} onChange={handleChange}>
-          <option value="">select</option>
+            <option value="">select</option>
             <option value="Admin">Admin</option>
             <option value="User">User</option>
           </select>
+          {errors.role && <div className="error">{errors.role}</div>}
         </div>
 
         <div className="position-center">
